923c6d354c6c3ce32675f4902163cb54408590b5
unknown <danielafuentesvaca@gmail.com>
2025-09-03 18:04:58 -0700

    Inicio

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,12 @@
 import reactLogo from "@/assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./index.css";
+import Inicio from "./inicio/Inicio";
 
 export default function App() {
   return (
-    <div className="min-h-screen grid place-items-center gap-6">
-      <div className="flex items-center gap-6">
-        <img src={viteLogo} alt="Vite" className="h-12" />
-        <img src={reactLogo} alt="React" className="h-12" />
-      </div>
-      <h1 className="text-3xl font-bold text-blue-600">transporte-si2 — Frontend OK 🚀</h1>
+    <div>
+      <Inicio/>
     </div>
   );
 }
